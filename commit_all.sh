9b11155d76bd6f0b676d89f021fde078d73f3807
Alexander Granin <graninas@gmail.com>
2018-02-07 01:17:57 +0700

    STM WIP.

--- commit_all.sh
+++ commit_all.sh
@@ -1,2 +1,22 @@
 git add -A
-git commit -a -m "WIP."
+git commit -a -m "STM WIP."
+
+cd ./lib/cpp_functional_core
+
+git add -A
+git commit -a -m "STM WIP."
+
+cd ../lib/cpp_lenses
+
+git add -A
+git commit -a -m "STM WIP."
+
+cd ../lib/cpp_monads
+
+git add -A
+git commit -a -m "STM WIP."
+
+cd ../lib/cpp_stm
+
+git add -A
+git commit -a -m "STM WIP."
